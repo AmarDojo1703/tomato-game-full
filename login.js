6297ec3bac1417579292244e87c9e0aa280d5170
Amar Kundal <Amar.kundal1703@gmail.com>
2023-12-07 15:26:55 +0530

    added login page
    added login page to implement the virtual identity.

---
+++ login.js
@@ -0,0 +1,18 @@
+const loginForm = document.getElementById("loginForm");
+
+const onLoginFormSubmit = (e) => {
+  e.preventDefault();
+  let loginData = new FormData(loginForm);
+  if (
+    !loginData.get("username") === "test" ||
+    !loginData.get("password") === "test123"
+  ) {
+    alert("Invalid username or password!");
+    return;
+  }
+
+  document.cookie = "username=test; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+  location.href = "index.html";
+};
+
+loginForm.addEventListener("submit", onLoginFormSubmit);
